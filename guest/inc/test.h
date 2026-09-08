543e9538fbf3e4212ac0d861383ef3fb43dc3e03
blin04 <ivanbokor17@gmail.com>
2026-09-08 11:20:42 +0200

    move tests into separate files

---
+++ guest/inc/test.h
@@ -0,0 +1,10 @@
+#ifndef TEST_H
+#define TEST_H
+
+void fileio_test(void);
+void write_test(void);
+void read_test(void);
+void lseek_test(void);
+void copy_on_write_test(void);
+
+#endif /* TEST_H */
